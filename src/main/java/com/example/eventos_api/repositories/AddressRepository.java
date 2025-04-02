3f1723068de9c79e3e1a1a4d9de1b14c0529d250
Bruna Feyh <bruna.feyh@unioeste.br>
2025-04-02 16:39:54 -0300

    refact: remove tests

--- src/main/java/com/example/eventos_api/repositories/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.eventos_api.repositories;
-
-import com.example.eventos_api.domain.address.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AddressRepository extends JpaRepository<Address, UUID> {
-}
--- src/main/java/com/example/eventos_api/repositories/CouponRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.eventos_api.repositories;
-
-import com.example.eventos_api.domain.coupon.Coupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CouponRepository extends JpaRepository<Coupon, UUID> {
-}
